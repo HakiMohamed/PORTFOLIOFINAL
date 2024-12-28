8a2ba383d2008d503c78c0d146993edc54b3ea69
Mohamed Haki <Mohamedhaki70@gmail.com>
2024-12-28 18:32:00 +0100

    final-3

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['@mui/material', '@emotion/react', '@emotion/styled'],
+          animations: ['framer-motion'],
+          icons: ['react-icons', '@heroicons/react']
+        }
+      }
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
 })
